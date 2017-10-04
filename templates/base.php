0337e616e69f17f6b29a535fe2765715750ce1a2
Floris <info@styxit.com>
2017-10-04 10:19:17 +0200

    Update project url

--- templates/base.php
+++ templates/base.php
@@ -20,7 +20,7 @@
     <div class="section">
         <div class="status-footer">
             <p>
-                <a href="https://github.com/styxit/mc-status"><i class="fa fa-github"></i> View on GitHub</a>
+                <a href="https://github.com/styxit/MinecraftStatusPage"><i class="fa fa-github"></i> View on GitHub</a>
                 <a href="https://styxit.com"><i class="fa fa-user-circle"></i> Created by Styxit</a>
             </p>
         </div>
